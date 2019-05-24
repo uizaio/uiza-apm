3b8d412f10ea42f2790fc8172a788794c1c0b638
deathemperor <deathemperor@gmail.com>
2019-05-24 12:07:18 +0700

    fixed an apm filter bug https://github.com/elastic/apm-agent-nodejs/issues/290

--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ class UizaApmAgent {
       apm.start(config);
     }
     apm.addFilter(function (payload) {
-      if (payload.context.request.url.pathname.indexOf("health_check") === -1) {
-        return payload;
-      } else {
-        if (config.logLevel) {
-          apm.logger.debug("health check request will not be recorded ", payload.context.request.url.pathname)
+      if (payload.context) { // issue here https://github.com/elastic/apm-agent-nodejs/issues/290
+        if (payload.context.request.url.pathname.indexOf("health_check") === -1) {
+          return payload;
+        } else {
+          if (config.logLevel) {
+            apm.logger.debug("health check request is not be recorded ", payload.context.request.url.pathname);
+          }
         }
+      } else {
+        return payload;
       }
     });
     return apm;
